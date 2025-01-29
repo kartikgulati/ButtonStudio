fa3796623af88b92e05e70ca03930e1a252d4400
kartikgulati <kartikgulati19@gmail.com>
2025-01-28 21:41:59 -0500

    addded new section in the feature section

--- src/components/ui/FeatureCard.jsx
+++ src/components/ui/FeatureCard.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 const FeatureCard = ({ icon, title, description }) => {
   return (
+    <div>
+      
     <div className="text-center p-6 rounded-xl hover:shadow-xl transition-shadow-hover shadow-white">
       <div className="text-indigo-600 mb-4 flex justify-center">{icon}</div>
       <h3 className="text-xl font-semibold mb-4">{title}</h3>
       <p className="text-gray-600 text-white">{description}</p>
     </div>
+
+
+    </div>
+    
   );
 };
 
