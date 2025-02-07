a90bb1113d435f9d5ab261aec1f6af59b9170e7c
kartikgulati <kartikgulati19@gmail.com>
2025-02-07 16:24:12 -0500

    changes in the navigation buttons

--- src/components/ui/ButtonShowcaseData.jsx
+++ src/components/ui/ButtonShowcaseData.jsx
@@ -123,13 +123,13 @@ const ButtonShowcaseData = () => {
   </span>
   <div className="inline-flex mt-2 xs:mt-0">
     
-  <button className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-white rounded-s hover:bg-gray-900 dark:bg-gray-200 dark:border-gray-700 dark:text-gray-800 dark:hover:bg-gray-700 dark:hover:text-white" onClick={handlePrevious} disabled={currentPage === 1}>
+  <button className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-white rounded-s hover:bg-gray-900 dark:bg-[#001524] dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white border border-gray-200" onClick={handlePrevious} disabled={currentPage === 1}>
         <svg className="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
         </svg>
         Prev
     </button>
-    <button className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-white border-0 border-s border-gray-200 rounded-e hover:bg-gray-900 dark:bg-gray-200 dark:border-gray-700 dark:text-gray-800 dark:hover:bg-gray-700 dark:hover:text-white" onClick={handleNext} disabled={currentPage === totalPages}>
+    <button className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-white border border-gray-200 rounded-e hover:bg-gray-900 dark:bg-[#001524] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white " onClick={handleNext} disabled={currentPage === totalPages}>
         Next
         <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
